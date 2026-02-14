038e250b95675604564cd809a3ebb9ce3e493987
Titik Rahmawati <titikr680@gmail.com>
2026-02-14 19:59:22 +0700

    cart-items updated

--- app/(landing)/components/checkout/cart-items.tsx
+++ app/(landing)/components/checkout/cart-items.tsx
@@ -1,65 +1,85 @@
 "use client";
 
-import { cartList } from "../ui/cart-popup";
 import Image from "next/image";
+import priceFormatter from "@/app/utils/price-formatter";
 import Button from "../ui/button";
 import { FiCreditCard, FiTrash2 } from "react-icons/fi";
-import PriceFormatter from "@/app/utils/price-formatter";
 import CardWithHeader from "../ui/card-with-header";
 import { useRouter } from "next/navigation";
+import { useCartStore } from "@/app/hooks/use-cart-store";
+import { getImageUrl } from "@/app/lib/api";
 
-const CartItems = () => {
-    const {push} = useRouter();
+type TCartItems = {
+  handlePayment: () => void;
+};
 
-    const totalPrice = cartList.reduce(
-        (total, item) => total + item.price * item.qty, 0
-    );
+const CartItems = ({ handlePayment }: TCartItems) => {
+  const { items, removeItem } = useCartStore();
+  const { push } = useRouter();
 
-    const Payment = () => {
+  const totalPrice = items.reduce(
+    (total, item) => total + item.price * item.qty,
+    0
+  );
 
-    }
-
-    return (
+  return (
     <CardWithHeader title="Cart Items">
-        <div className="overflow-auto max-h-[300px]">
-            {cartList.map((item, index) => (
-                <div key={index} className="border-b border-gray-200 p-4 flex gap-3">
-                    <div className="bg-primary-light aspect-square w-16 flex justify-center items-center">
-                        <Image
-                        src={`/images/products/${item.imgUrl}`}
-                        width={63}
-                        height={63}
-                        alt={item.name}
-                        className="aspect-square object-contain"
-                        />
-                    </div>
-                    <div className="self-center flex flex-col">
-                        <div className="text-sm font-medium">{item.name}</div>
-                        <div className="flex gap-3 font-medium text-xs">
-                            <div>{item.qty}x</div>
-                            <div className="text-primary">{PriceFormatter(item.price)}</div>
-                            </div>
-                        </div>
-                        <Button
-                        size="small"
-                        variant="ghost"
-                        className="w-7 h-7 p-0! self-center ml-auto">
-                        <FiTrash2 />
-                        </Button>
-                    </div>
-                ))}
+      <div className="flex flex-col justify-between h-[calc(100%-70px)]">
+        <div className="overflow-auto max-h-[300px] ">
+          {items.map((item) => (
+            <div
+              className="border-b border-gray-200 p-4 flex gap-3"
+              key={item._id}
+            >
+              <div className="bg-primary-light aspect-square w-16 flex justify-center items-center">
+                <Image
+                  src={getImageUrl(item.imageUrl)}
+                  width={63}
+                  height={63}
+                  alt={item.name}
+                  className="aspect-square object-contain"
+                />
+              </div>
+              <div className="self-center">
+                <div className="text-sm font-medium">{item.name}</div>
+                <div className="flex gap-3 font-medium text-xs">
+                  <div>{item.qty}x</div>
+                  <div className="text-primary">
+                    {priceFormatter(item.price)}
+                  </div>
                 </div>
-            <div className="border-t border-gray-200 p-4">
-                <div className="flex justify-between font-semibold">
-                    <div className="text-sm">Total</div>
-                    <div className="text-primary text-xs">{PriceFormatter(totalPrice)}</div>
-                </div>
-                <Button variant="dark" className="w-full mt-4" onClick={() => push ("/payment")}>
-                <FiCreditCard /> Proceed to Payment
-                </Button>
+              </div>
+              <Button
+                size="small"
+                variant="ghost"
+                className="w-7 h-7 p-0! self-center ml-auto"
+                onClick={() => removeItem(item._id)}
+              >
+                <FiTrash2 />
+              </Button>
             </div>
-        </CardWithHeader>
-    );
+          ))}
+        </div>
+
+        <div className="border-t border-gray-200 p-4">
+          <div className="flex justify-between font-semibold">
+            <div className="text-sm">Total</div>
+            <div className="text-primary text-xs">
+              {priceFormatter(totalPrice)}
+            </div>
+          </div>
+          <Button
+            variant="dark"
+            className="w-full mt-4"
+            onClick={handlePayment}
+          >
+            <FiCreditCard />
+            Proceed to Payment
+          </Button>
+        </div>
+      </div>
+    </CardWithHeader>
+  );
 };
 
 export default CartItems;
