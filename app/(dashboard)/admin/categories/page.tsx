eb03efbe0ce685f73c2089a18604e9785c04e20b
Titik Rahmawati <titikr680@gmail.com>
2026-02-20 11:44:28 +0700

    Session 4

---
+++ app/(dashboard)/admin/categories/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Button from "@/app/(landing)/components/ui/button";
+import { FiPlus } from "react-icons/fi";
+import { useState } from "react";
+import CategoryTable from "../../components/categories/category-table";
+import CategoryModal from "../../components/categories/category-modal";
+
+const CategoryManagement = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <div className="flex justify-between items-center mb-10">
+        <div>
+          <h1 className="font-bold text-2xl">Category Management</h1>
+          <p className="opacity-50">Organize your products into categories.</p>
+        </div>
+        <Button className="rounded-lg" onClick={() => setIsOpen(true)}>
+          <FiPlus size={24} />
+          Add Category
+        </Button>
+      </div>
+      <CategoryTable />
+      <CategoryModal isOpen={isOpen} onClose={handleCloseModal} />
+    </div>
+  );
+};
+
+export default CategoryManagement;
--- app/(landing)/checkout/page.tsx
+++ app/(landing)/checkout/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import { JSX, useState } from "react";
 import CartItems from "../components/checkout/cart-items";
 import OrderInformation from "../components/checkout/order-information";
 import { CustomerInfo, useCartStore } from "@/app/hooks/use-cart-store";
 import { useRouter } from "next/navigation";
 
-const Checkout = () => {
+const Checkout = (): JSX.Element => {
   const { push } = useRouter();
   const { customerInfo, setCustomerInfo } = useCartStore();
   const [formData, setFormData] = useState<CustomerInfo>({
@@ -34,7 +34,10 @@ const Checkout = () => {
       <div className="max-w-5xl mx-auto py-20">
         <h1 className="text-5xl font-bold text-center mb-11">Checkout Now</h1>
         <div className="grid grid-cols-2 gap-14">
-          <OrderInformation formData={formData} setFormData={setFormData} />
+          <OrderInformation
+            formData={formData}
+            setFormData={setFormData}
+          />
           <CartItems handlePayment={handlePayment} />
         </div>
       </div>
