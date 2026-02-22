69280645777d13f47f7e2f207aa9a599368d3e79
Titik Rahmawati <titikr680@gmail.com>
2026-02-22 19:46:58 +0700

    session 3

--- app/(landing)/components/payment/payment-options.tsx
+++ app/(landing)/components/payment/payment-options.tsx
@@ -1,44 +1,47 @@
 import { FiCreditCard } from "react-icons/fi";
 import CardWithHeader from "../ui/card-with-header";
+import { getAllBanks } from "@/app/services/bank.service";
 
 const paymentList = [
-    {
-        bank_name: "BCA",
-        account_number: "0123182312",
-        account_holder: "PT SportOn Digital",
-    },
-    {
-        bank_name: "Mandiri",
-        account_number: "83923912013203123",
-        account_holder: "PT SportOn Digital",
-    },
-    {
-        bank_name: "BTPN",
-        account_number: "5238218923",
-        account_holder: "PT SportOn Digital",
-    }
-]
+  {
+    bank_name: "BCA",
+    account_number: 1231231231231,
+    account_holder: "PT SportsOn Digital",
+  },
+  {
+    bank_name: "Mandiri",
+    account_number: 89458434,
+    account_holder: "PT SportsOn Digital",
+  },
+  {
+    bank_name: "BRI",
+    account_number: 123891283912,
+    account_holder: "PT SportsOn Digital",
+  },
+];
 
-const PaymentOptions = () => {
-    return (
-        <CardWithHeader title="Payment Options">
-            {
-                paymentList.map((payment, index) => (
-                    <div className="flex gap-5 p-5 border-b border-gray-200" key={index}>
-                        <div className="bg-blue-100 p-4 text-blue-500 h-fit self-center">
-                            <FiCreditCard size={24} />
-                        </div>
-                        <div className="self-center">
-                            <div className="font-bold">{payment.bank_name}</div>
-                            <div className="text-sm">{payment.account_number}</div>
-                            <div className="text-sm opacity-70">{payment.account_holder}</div>
-                        </div>
-                        <div className="ml-auto bg-blue-50 text-gray-800 h-fit self-center text-xs px-2 py-1">Bank Transfer</div>
-                    </div>
-                ))
-            }
-        </CardWithHeader>
-    )
-}
+const PaymentOptions = async () => {
+  const banks = await getAllBanks();
+
+  return (
+    <CardWithHeader title="Payment Options">
+      {banks.map((payment, index) => (
+        <div className="flex gap-5 p-5 border-b border-gray-100" key={index}>
+          <div className="bg-blue-100 p-4 text-blue-500 h-fit self-center">
+            <FiCreditCard size={24} />
+          </div>
+          <div className="self-center">
+            <div className="font-bold">{payment.bankName}</div>
+            <div className="text-sm">{payment.accountNumber}</div>
+            <div className="text-sm opacity-70">{payment.accountName}</div>
+          </div>
+          <div className="ml-auto bg-blue-50 text-gray-800 text-xs h-fit self-center px-2 py-1">
+            Bank Transfer
+          </div>
+        </div>
+      ))}
+    </CardWithHeader>
+  );
+};
 
 export default PaymentOptions;
